5fb088ffb35c7d3ac8096f166cb58b6c98f79596
sb13910359 <j39457874@gmail.com>
2025-10-18 20:03:38 +1100

    Update sketch_oct18a.ino with comments

--- sketch_oct18a.ino
+++ sketch_oct18a.ino
@@ -1,8 +1,8 @@
-const int buttonPin = 2;
+const int buttonPin = 2;      // Connect button to digital pin 2
 bool lastState = HIGH;
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP);      // Internal pull up resistor
   Serial.begin(9600);
 }
 
@@ -10,11 +10,11 @@ void loop() {
   bool state = digitalRead(buttonPin);
 
   if (state == LOW && lastState == HIGH) {
-    Serial.println("ESTOP");
+    Serial.println("ESTOP");          // prints "ESTOP" for system to read via serial
     lastState = LOW;
   } 
   else if (state == HIGH && lastState == LOW) {
-    lastState = HIGH;
+    lastState = HIGH;                  // Makes sure ESTOP is only triggered once for one button press
   }
 
   delay(50);
