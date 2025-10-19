067635491cee5a68ff64568a8d9141841035a7c8
sb13910359 <j39457874@gmail.com>
2025-10-19 20:00:12 +1100

    Update estop_sketch.ino to match gui estop buttons

--- estop_sketch.ino
+++ estop_sketch.ino
@@ -1,6 +1,6 @@
 const int buttonPins[3] = {2, 3, 4};   // Button1: Digital pin2, Button2:digital pin3, Button3:Digital pin4
-
-bool lastStates[3] = {HIGH, HIGH, HIGH};
+bool lastStates[3] = {HIGH, HIGH, HIGH};  
+int buttonStage[3] = {0, 0, 0};           // 0=normal, 1= estopactive, 2=confirm release estop 
 
 void setup() {
   Serial.begin(9600);
@@ -14,12 +14,28 @@ void loop() {
     bool state = digitalRead(buttonPins[i]);
 
     if (state == LOW && lastStates[i] == HIGH) {
-      Serial.print("ESTOP_R");    // prints "ESTOP" for system to read via serial
-      Serial.println(i + 1);      // e.g. ESTOP_R1 / ESTOP_R2 / ESTOP_R3
+      buttonStage[i] = (buttonStage[i] + 1) % 3;    //change the button stage
+
+      if (buttonStage[i] == 1) {
+        Serial.print("ESTOP_R");   // prints "ESTOP" for system to read via serial
+        Serial.println(i + 1);     // e.g ESTOP_R1 / ESTOP_R2 / ESTOP_R3
+      } 
+
+      else if (buttonStage[i] == 2) {
+        Serial.print("CONFIRM_R");  // confirm release?
+        Serial.println(i + 1);    
+      }
+
+      else if (buttonStage[i] == 0) {
+        Serial.print("CLEAR_R");  // Clear estop
+        Serial.println(i + 1);   
+      }
+
       lastStates[i] = LOW;
-    } 
+    }
+
     else if (state == HIGH && lastStates[i] == LOW) {
-      lastStates[i] = HIGH;      // Makes sure ESTOP is only triggered once for one button press
+      lastStates[i] = HIGH;                          // Makes sure ESTOP is only triggered once for one button press
     }
   }
 
