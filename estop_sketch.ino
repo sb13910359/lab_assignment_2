74e2c1ebe5ecf5a2c145740f7b08dea5411f9e94
sb13910359 <j39457874@gmail.com>
2025-10-19 15:40:01 +1100

    Update estop_sketch.ino to add multiple (3) hardware buttons

--- estop_sketch.ino
+++ estop_sketch.ino
@@ -1,20 +1,26 @@
-const int buttonPin = 2;      // Connect button to digital pin 2
-bool lastState = HIGH;
+const int buttonPins[3] = {2, 3, 4};   // Button1: Digital pin2, Button2:digital pin3, Button3:Digital pin4
+
+bool lastStates[3] = {HIGH, HIGH, HIGH};
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);      // Internal pull up resistor
   Serial.begin(9600);
+  for (int i = 0; i < 3; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);   // Internal pull up resistor
+  }
 }
 
 void loop() {
-  bool state = digitalRead(buttonPin);
+  for (int i = 0; i < 3; i++) {
+    bool state = digitalRead(buttonPins[i]);
 
-  if (state == LOW && lastState == HIGH) {
-    Serial.println("ESTOP");          // prints "ESTOP" for system to read via serial
-    lastState = LOW;
-  } 
-  else if (state == HIGH && lastState == LOW) {
-    lastState = HIGH;                  // Makes sure ESTOP is only triggered once for one button press
+    if (state == LOW && lastStates[i] == HIGH) {
+      Serial.print("ESTOP_R");    // prints "ESTOP" for system to read via serial
+      Serial.println(i + 1);      // e.g. ESTOP_R1 / ESTOP_R2 / ESTOP_R3
+      lastStates[i] = LOW;
+    } 
+    else if (state == HIGH && lastStates[i] == LOW) {
+      lastStates[i] = HIGH;      // Makes sure ESTOP is only triggered once for one button press
+    }
   }
 
   delay(50);
